e3c46985d8d9eb3a102f0f22f2a9d8137a205a9f
Vishal <vishal.a.bhalla@gmail.com>
2015-12-02 22:07:54 +0100

    Initial setup - Pre-processing Tweets.

---
+++ Twitter User Personality Prediction/PreprocessTweets.py
@@ -0,0 +1,28 @@
+import re
+
+class PreprocessTweets:
+
+    def __init__(self):
+        self.name = 'PreprocessTweets'
+
+    #start process_tweet
+    def processTweet(self, tweet):
+        # process the tweets
+
+        #Convert to lower case
+        tweet = tweet.lower()
+        #Convert www.* or https?://* to URL
+        tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','URL',tweet)
+        #Convert @username to AT_USER
+        tweet = re.sub('@[^\s]+','AT_USER',tweet)
+        #Remove additional white spaces
+        tweet = re.sub('[\s]+', ' ', tweet)
+        #Remove special characters
+        #tweet = re.sub('*\[\]%\(\)', '', tweet)
+        #Replace #word with word
+        tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+        #trim
+        tweet = tweet.strip('\'"')
+        return tweet
+    #end
+
